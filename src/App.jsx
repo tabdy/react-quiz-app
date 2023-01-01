a6293357625302961743d6cd03856cbcbae064d9
Timur Abd <timur_dev@yahoo.com>
2023-01-02 02:00:37 +0300

    Added functionality to check the number of correct answers

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import QuizItem from './components/QuizItem'
 function App() {
   const [isStarted, setIsStarted] = React.useState(false)
   const [quizData, setQuizData] = React.useState([])
+  const [quizResult, setQuizResult] = React.useState("")
+  const [isFinished, setIsFinished] = React.useState(false)
   console.log(quizData)
 
   React.useEffect(() => {
@@ -14,26 +16,51 @@ function App() {
         .then(resp => resp.json())
         .then((data) => {
           setQuizData(data.results)
-          // console.log(data.results)
         })
     }
   }, [isStarted])
 
   function startQuiz() {
     setIsStarted(true)
-    console.log("clicked")
   }
 
-   const quizBlocks = quizData.map(dataObj => {
-    // console.log(dataObj)
+   const quizBlocks = quizData.map((dataObj, i) => {
+    // console.log("obj", dataObj)
     return (
-      <QuizItem 
+      <QuizItem
+      key={i}
+      id={i}
       question={dataObj.question}
       incorrectAnswers={dataObj.incorrect_answers}
       correctAnswer={dataObj.correct_answer}
+      setChosen={setChosen}
+      isFinished={isFinished}
        />
     )
   })
+
+  function setChosen(id, choice) {
+   
+    setQuizData(prevState => {
+      return prevState.map((dataObj, i) => {
+        return i === id ? {...dataObj, choozen: choice} : dataObj
+      })
+      
+    })
+  }
+
+
+  function checkAnswers() { 
+   if (quizData.filter((dataObj) => dataObj.choozen).length < 5) {
+    setQuizResult("Please answer all the questions!")
+   } else {
+    let result = quizData.filter((dataObj) => dataObj.choozen === dataObj.correct_answer).length
+    setQuizResult(`You scored ${result}/5 correct answers`)
+    setIsFinished(true)
+   }
+
+  }
+
 
   return (
     <div className="App">
@@ -58,7 +85,10 @@ function App() {
         
         <div className='quize-container'>
           {quizBlocks}
-          <button className='quize-container__button'>Check answers</button>
+          <div className='quize-container__check-results'>
+            {quizResult !== "" && <h3>{quizResult}</h3>}
+            <button className='quize-container__button' onClick={checkAnswers}>Check answers</button>
+          </div>
         </div>
       }
 
--- src/components/QuizItem.jsx
+++ src/components/QuizItem.jsx
@@ -5,33 +5,54 @@ import {decode} from 'html-entities'
 function QuizItem(props) {
   const [randomIndexArr, setRandomIndexArr] = React.useState(() => randomAnswer())
   const answers = [...props.incorrectAnswers, props.correctAnswer]
-  console.log(randomIndexArr)
+  const [chosenAnswer, setChosenAnswer] = React.useState("")
+  console.log(chosenAnswer)
 
   function randomAnswer() {
 
     const indexNum = []
-
     while (indexNum.length < 4) {
-      let randomNum = Math.floor(Math.random()*5)
+      let randomNum = Math.floor(Math.random() * 5)
       if (indexNum.every(num => randomNum !== num)) {
         indexNum.push(randomNum)
-      }   
+      }
     }
-
     return indexNum;
 
   }
 
+  function buttonPressed(i) {
+    // console.log("pressed", i)
+    setChosenAnswer(answers[i])
+    // console.log(props.id, answers[i])
+    props.setChosen(props.id, answers[i])
+  }
 
+  const AnswersОptions = answers.map((answer, i) => (
+    <button
+    key={i}
+    className={'quize-container__answer' + 
+      (chosenAnswer === answer ? ' quize-container__answer_selected' : '')
+      +
+      (props.isFinished && chosenAnswer === answer ? ' quize-container__answer_selected-wrong' : '')
+      +
+      (props.isFinished && props.correctAnswer === answer ? ' quize-container__correct-answer' : '')
+    }
+    onClick={(e) => buttonPressed(i)}>{decode(answer)}
+    </button>
+  ))
+  
+  
 
     return (
         <div className='quize-container__item'>
         <h2>{decode(props.question)}</h2>
         <div className='quize-container__answers-block'>
-          <div className='quize-container__answer'>{decode(answers[0])}</div>
+        {AnswersОptions}
+          {/* <div className='quize-container__answer'>{decode(answers[0])}</div>
           <div className='quize-container__answer'>{decode(answers[1])}</div>
           <div className='quize-container__answer'>{decode(answers[2])}</div>
-          <div className='quize-container__answer'>{decode(answers[3])}</div>
+          <div className='quize-container__answer'>{decode(answers[3])}</div> */}
         </div>
         <hr />
       </div>
